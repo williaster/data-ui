29823482139253838aa0b589bb5434a78f12a9b6
Chris Williams <chris.williams@airbnb.com>
2017-04-21 11:16:31 -0700

    add simple Table test and add test script to root package + publish flow

--- packages/demo/examples/index.jsx
+++ packages/demo/examples/index.jsx
@@ -7,7 +7,7 @@ requireContext.keys().forEach((packageName) => {
   if (packageName !== 'shared') {
     const examples = requireContext(packageName);
     if (examples && examples.default) {
-      const name = path.dirname(packageName).slice(2);
+      const name = path.dirname(packageName).slice(2); // no './'
       const stories = storiesOf(name, module);
       examples.default.forEach((example) => {
         stories.add(example.description, example.example);
