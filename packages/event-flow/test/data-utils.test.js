27f3d2045201c95c5f31bba8f82f91011fae418a
Chris Williams <chris.williams@airbnb.com>
2017-06-20 23:19:23 -0700

    [event-flow] utils tests to working again

--- packages/event-flow/test/data-utils.test.js
+++ packages/event-flow/test/data-utils.test.js
@@ -9,30 +9,34 @@ import {
   binEventsByEntityId,
   findNthIndexOfX,
   createEvent,
+  cleanEvents,
+  collectSequencesFromNode,
 } from '../src/utils/data-utils';
 
-describe('createEvent', () => {
+describe('cleanEvents', () => {
   const accessors = {
-    [TS]: () => 'ts',
-    [ENTITY_ID]: () => 'entity id',
-    [EVENT_NAME]: () => 'event name',
+    [TS]: d => d.ts,
+    [ENTITY_ID]: d => d.id,
+    [EVENT_NAME]: d => d.name,
   };
 
+  const dirty = [
+    { i: 'am', a: 'raw', event: ['!!!'], ts: 3, id: 'id', name: 'name' },
+    { i: 'am', a: 'raw', event: ['!!!'], ts: 1, id: 'id', name: 'name' },
+    { i: 'am', a: 'raw', event: ['!!!'], ts: 2, id: 'id', name: 'name' },
+  ];
+
+  const clean = cleanEvents(dirty, accessors);
+
   test('it should be defined', () => {
-    expect(createEvent).toBeDefined();
+    expect(cleanEvents).toBeDefined();
   });
 
-  test('it should add TS, ENTITY_ID, EVENT_NAME, META keys to the event', () => {
-    const e = createEvent({}, accessors);
-    [TS, ENTITY_ID, EVENT_NAME, META].forEach((prop) => {
-      expect(e).toHaveProperty(prop);
-    });
-  });
-
-  test('it should assign values using the passed accessor fns', () => {
-    const e = createEvent({}, accessors);
-    [TS, ENTITY_ID, EVENT_NAME].forEach((prop) => {
-      expect(e).toHaveProperty(prop, accessors[prop]());
+  test('it should add TS, ENTITY_ID, EVENT_NAME, META keys to the events', () => {
+    clean.forEach((e) => {
+      [TS, ENTITY_ID, EVENT_NAME, META].forEach((prop) => {
+        expect(e).toHaveProperty(prop);
+      });
     });
   });
 
@@ -40,6 +44,20 @@ describe('createEvent', () => {
     const rawEvent = { i: 'am', a: 'raw', event: ['!!!'] };
     const e = createEvent(rawEvent, accessors);
     expect(e[META]).toEqual(rawEvent);
+  });
+
+  test('it should sort by ts', () => {
+    expect(clean[0][TS]).toBe(1);
+    expect(clean[1][TS]).toBe(2);
+    expect(clean[2][TS]).toBe(3);
+  });
+
+  test('it should assign values using the passed accessor fns', () => {
+    clean.forEach((e) => {
+      [TS, ENTITY_ID, EVENT_NAME].forEach((prop) => {
+        expect(e).toHaveProperty(prop, accessors[prop](e[META]));
+      });
+    });
   });
 });
 
@@ -59,6 +77,26 @@ describe('binEventsByEntityId', () => {
     expect(result.u1).toEqual(u1);
     expect(result.u2).toEqual(u2);
     expect(result.u3).toEqual(u3);
+  });
+
+  test('it should return an object', () => {
+    expect(typeof binEventsByEntityId([])).toBe('object');
+  });
+
+  test('it should return events for each unique entity', () => {
+    const result = binEventsByEntityId([
+      { [ENTITY_ID]: 1 },
+      { [ENTITY_ID]: 1 },
+      { [ENTITY_ID]: 2 },
+      { [ENTITY_ID]: 4 },
+    ]);
+    expect(Object.keys(result).length).toBe(3);
+  });
+
+  test('it should return an object of arrays', () => {
+    const result = binEventsByEntityId([{ [ENTITY_ID]: 1 }, { [ENTITY_ID]: 2 }]);
+    expect(Array.isArray(result[1])).toBe(true);
+    expect(Array.isArray(result[2])).toBe(true);
   });
 });
 
@@ -92,6 +130,7 @@ describe('findNthIndexOfX', () => {
     expect(findNthIndexOfX(things, -1, e => e.name === 'a')).toBe(things.length - 1);
     expect(findNthIndexOfX(things, -1, e => e.name === 'b')).toBe(3);
     expect(findNthIndexOfX(things, -1, e => e.name === 'c')).toBe(4);
+    expect(findNthIndexOfX(things, 7, () => true)).toBe(6);
   });
 
   test('it should find the 2nd to last occurrence of an event', () => {
@@ -113,5 +152,41 @@ describe('findNthIndexOfX', () => {
   test('it should return -1 for when occurrence is outside of the array length range', () => {
     expect(findNthIndexOfX(things, 10, () => true)).toBe(-1);
     expect(findNthIndexOfX(things, -10, () => true)).toBe(-1);
+  });
+});
+
+describe('collectSequencesFromNode', () => {
+  const events = {
+    u1: [1, 2, 3],
+    u2: [4, 5, 6],
+  };
+
+  const node = {
+    events: {
+      e1: {
+        [ENTITY_ID]: 'u1',
+      },
+      e2: {
+        [ENTITY_ID]: 'u2',
+      },
+      e3: {
+        [ENTITY_ID]: 'u1',
+      },
+    },
+  };
+
+  test('it should be defined', () => {
+    expect(collectSequencesFromNode).toBeDefined();
+  });
+
+  test('it should return a sequence for each unique entity key', () => {
+    const sequences = collectSequencesFromNode(node, events);
+    expect(sequences.length).toBe(2);
+  });
+
+  test('it should collect sequences for each unique entity key', () => {
+    const sequences = collectSequencesFromNode(node, events);
+    expect(sequences).toContain(events.u1);
+    expect(sequences).toContain(events.u2);
   });
 });
--- packages/event-flow/test/graph-utils.test.js
+++ packages/event-flow/test/graph-utils.test.js
@@ -8,15 +8,13 @@ import {
 } from '../src/constants';
 
 import {
-  binEventsByEntityId,
-} from '../src/utils/data-utils';
-
-import {
   buildNodesFromEntityEvents,
   buildGraph,
   getNodeFromEvent,
   addMetaDataToNodes,
-  createRoot,
+  getRoot,
+  getNodeId,
+  ancestorsFromNode,
 } from '../src/utils/graph-utils';
 
 const user1 = [
@@ -34,32 +32,6 @@ const user2 = [
   { [TS]: new Date('2017-03-22 18:37:10'), [EVENT_NAME]: 'd', [ENTITY_ID]: 'u2' },
 ];
 
-describe('binEventsByEntityId', () => {
-  test('it should be defined', () => {
-    expect(binEventsByEntityId).toBeDefined();
-  });
-
-  test('it should return an object', () => {
-    expect(typeof binEventsByEntityId([])).toBe('object');
-  });
-
-  test('it should return events for each unique entity', () => {
-    const result = binEventsByEntityId([
-      { [ENTITY_ID]: 1 },
-      { [ENTITY_ID]: 1 },
-      { [ENTITY_ID]: 2 },
-      { [ENTITY_ID]: 4 },
-    ]);
-    expect(Object.keys(result).length).toBe(3);
-  });
-
-  test('it should return an object of arrays', () => {
-    const result = binEventsByEntityId([{ [ENTITY_ID]: 1 }, { [ENTITY_ID]: 2 }]);
-    expect(Array.isArray(result[1])).toBe(true);
-    expect(Array.isArray(result[2])).toBe(true);
-  });
-});
-
 describe('getNodeFromEvent', () => {
   test('it should be defined', () => {
     expect(getNodeFromEvent).toBeDefined();
@@ -70,15 +42,18 @@ describe('getNodeFromEvent', () => {
   });
 
   test('it should not initialize nodes that already exits', () => {
+    const depth = 0;
     const nodes = {};
-    const node = getNodeFromEvent(nodes, '123', 0);
+    const node = getNodeFromEvent(nodes, 'id', 'eventName', depth);
     nodes[node.id] = node;
 
-    expect(getNodeFromEvent(nodes, '123', 0)).toBe(nodes[node.id]);
-    expect(getNodeFromEvent(nodes, '123', 1)).not.toBe(nodes[node.id]);
-    expect(getNodeFromEvent(nodes, '1234', 0)).not.toBe(nodes[node.id]);
+    // id defines unique-ness
+    expect(getNodeFromEvent(nodes, 'id', 'eventName', depth)).toBe(node);
+    expect(getNodeFromEvent(nodes, 'id', 'eventName', depth + 1)).toBe(node);
+    expect(getNodeFromEvent(nodes, 'id2', 'eventName', depth)).not.toBe(node);
   });
 });
+
 
 describe('buildNodesFromEntityEvents', () => {
   test('it should be defined', () => {
@@ -89,87 +64,105 @@ describe('buildNodesFromEntityEvents', () => {
     const nodes = {};
     buildNodesFromEntityEvents(user1, 0, nodes);
     expect(Object.keys(nodes).length).toBe(4);
-    expect(Object.keys(nodes)).toEqual(expect.arrayContaining(['a__0', 'b__1', 'c__2', 'd__3']));
   });
 
   test('it should parse all events from the last event', () => {
     const nodes = {};
     buildNodesFromEntityEvents(user1, 3, nodes);
     expect(Object.keys(nodes).length).toBe(4);
-    expect(Object.keys(nodes)).toEqual(expect.arrayContaining(['a__-3', 'b__-2', 'c__-1', 'd__0']));
   });
 
   test('it should parse all events from an arbitrary event', () => {
     const nodes = {};
     buildNodesFromEntityEvents(user1, 2, nodes);
     expect(Object.keys(nodes).length).toBe(4);
-    expect(Object.keys(nodes)).toEqual(expect.arrayContaining(['a__-2', 'b__-1', 'c__0', 'd__1']));
   });
 
-  test('it should create one node for each unique (event name, event index) pair', () => {
+  test('it should create one node for each unique event sequence', () => {
     const nodes = {};
     buildNodesFromEntityEvents(user1, 0, nodes);
     expect(Object.keys(nodes).length).toBe(4);
-    expect(Object.keys(nodes)).toEqual(expect.arrayContaining(['a__0', 'b__1', 'c__2', 'd__3']));
 
     buildNodesFromEntityEvents(user1, 3, nodes); // all new indices
     expect(Object.keys(nodes).length).toBe(8);
-    expect(Object.keys(nodes)).toEqual(expect.arrayContaining(['a__-3', 'b__-2', 'c__-1', 'd__0']));
 
     buildNodesFromEntityEvents(user2, 0, nodes); // some overlapping nodes
     expect(Object.keys(nodes).length).toBe(11);
-    expect(Object.keys(nodes)).toEqual(expect.arrayContaining(['b__2', 'c__3', 'd__4']));
   });
 
   test('it should add proper parent and children references', () => {
+    const startIndex = 1;
     const nodes = {};
-    buildNodesFromEntityEvents(user1, 0, nodes); // => 'a__0', 'b__1', 'c__2', 'd__3'
+    buildNodesFromEntityEvents(user1, startIndex, nodes);
 
-    expect(nodes.a__0.parent).toBeFalsy();
-    expect(nodes.b__1.parent).toBe('a__0');
-    expect(nodes.c__2.parent).toBe('b__1');
-    expect(nodes.d__3.parent).toBe('c__2');
+    const ordered = Object.keys(nodes)
+      .sort((a, b) => nodes[a].depth - nodes[b].depth)
+      .map(nId => nodes[nId]);
 
-    expect(Object.keys(nodes.a__0.children)).toContain('b__1');
-    expect(Object.keys(nodes.b__1.children)).toContain('c__2');
-    expect(Object.keys(nodes.c__2.children)).toContain('d__3');
-    expect(Object.keys(nodes.d__3.children)).toEqual([]);
+    // negative depth nodes
+    expect(ordered[0].parent).toBe(ordered[1]);
+    expect(ordered[1].children[ordered[0].id]).toBe(ordered[0]);
+
+    // root
+    expect(ordered[1].parent).toBeFalsy();
+
+    // positive depth nodes
+    expect(ordered[1].children[ordered[2].id]).toEqual(ordered[2]);
+    expect(ordered[2].parent).toEqual(ordered[1]);
+
+    expect(ordered[2].children[ordered[3].id]).toEqual(ordered[3]);
+    expect(ordered[3].parent).toEqual(ordered[2]);
+
+    expect(ordered[3].children).toEqual({});
   });
 
   test('it should add proper depth attributes', () => {
+    const startIndex = 2;
     const nodes = {};
-    buildNodesFromEntityEvents(user1, 2, nodes); // => 'a__-2', 'b__-1', 'c__0', 'd__1'
+    buildNodesFromEntityEvents(user1, startIndex, nodes);
 
-    expect(nodes['a__-2'].depth).toBe(-2);
-    expect(nodes['b__-1'].depth).toBe(-1);
-    expect(nodes.c__0.depth).toBe(0);
-    expect(nodes.d__1.depth).toBe(1);
+    const depths = Object.keys(nodes).map(nId => nodes[nId].depth);
+    const expectedDepths = Array(user1.length).fill().map((_, i) => i - startIndex);
+
+    expect(depths).toEqual(expect.arrayContaining(expectedDepths));
   });
 
   test('it should add event referenecs to nodes', () => {
     const nodes = {};
     buildNodesFromEntityEvents(user1, 0, nodes);
 
-    // @todo don't hard code these
-    expect(nodes.a__0.events.a__0__u1).toMatchObject(user1[0]);
-    expect(nodes.b__1.events.b__1__u1).toMatchObject(user1[1]);
+    expect(Object.keys(nodes).length).toBe(user1.length);
+
+    // for each node
+    Object.keys(nodes).forEach((nId) => {
+      expect(Object.keys(nodes[nId].events).length).toBe(1);
+
+      // for all events on the node
+      Object.keys(nodes[nId].events).forEach((eId) => {
+        // expect event array to contain that event
+        const event = nodes[nId].events[eId];
+        expect(user1).toEqual(expect.arrayContaining([event]));
+      });
+    });
   });
 
   test('it should add TS0, TS_NEXT, and TS_PREV properties to events', () => {
     const nodes = {};
     buildNodesFromEntityEvents(user1, 0, nodes);
 
-    ['a__0', 'b__1', 'c__2', 'd__3'].forEach((nodeId) => {
-      const eventKeys = Object.keys(nodes[nodeId].events);
-      eventKeys.forEach((eventKey) => {
+    Object.keys(nodes).forEach((nId) => {
+      const eventKeys = Object.keys(nodes[nId].events);
+
+      eventKeys.forEach((eId) => {
         [TS0, TS_PREV, TS_NEXT].forEach((prop) => {
-          const event = nodes[nodeId].events[eventKey];
+          const event = nodes[nId].events[eId];
           expect(event).toHaveProperty(prop);
         });
       });
     });
   });
 });
+
 
 describe('addMetaDataToNodes', () => {
   test('it should be defined', () => {
@@ -180,23 +173,54 @@ describe('addMetaDataToNodes', () => {
   // validate elapsed times
 });
 
-describe('createRoot', () => {
+
+describe('getRoot', () => {
   test('it should be defined', () => {
-    expect(createRoot).toBeDefined();
+    expect(getRoot).toBeDefined();
   });
 
-  // @todo
-  // validate depth === 0 for all root.children keys
+  test('it should only add nodes with depth === 0 to the root', () => {
+    const nodes = {
+      1: { depth: -1 },
+      2: { depth: 0 },
+      3: { depth: 1 },
+    };
+    expect(getRoot(nodes).children).toEqual({ 2: { depth: 0 } });
+  });
 });
 
-describe('buildGraph', () => {
+
+describe('ancestorsFromNode', () => {
+  const startIndex = 1;
+  const nodes = {};
+  buildNodesFromEntityEvents(user1, startIndex, nodes);
+  const ordered = Object.keys(nodes).sort((a, b) => nodes[a].depth - nodes[b].depth);
+
   test('it should be defined', () => {
-    expect(buildGraph).toBeDefined();
+    expect(ancestorsFromNode).toBeDefined();
   });
 
-  test('it should keep a reference to events that are filtered', () => {
-    const nodes = buildGraph(user1, () => -1);
-    expect(nodes.filtered.u1).toEqual(user1);
+  test('it should return events in root -> child order', () => {
+    const key = ordered[ordered.length - 1];
+    const leaf = nodes[key];
+    const expectedEvents = ordered.slice(startIndex).map(k => nodes[k]);
+
+    expect(ancestorsFromNode(leaf)).toEqual(expectedEvents);
+  });
+
+  test('it should work for negative-depth nodes', () => {
+    const negativeKey = ordered[0];
+    const negativeNode = nodes[negativeKey];
+    const expectedEvents = ordered.slice(0, startIndex + 1).map(k => nodes[k]).reverse();
+
+    expect(ancestorsFromNode(negativeNode)).toEqual(expectedEvents);
+  });
+
+  test('it should return just the node if it is a root node', () => {
+    const key = ordered[startIndex];
+    const root = nodes[key];
+
+    expect(ancestorsFromNode(root)).toEqual([root]);
   });
 });
 
