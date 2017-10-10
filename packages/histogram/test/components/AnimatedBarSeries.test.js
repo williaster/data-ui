89f592e964c7e4814a6c8990decd44619b36c405
Chris Williams <chris.williams@airbnb.com>
2017-10-09 23:17:35 -0700

    [histogram][lint] fix

--- packages/histogram/test/components/AnimatedBarSeries.test.js
+++ packages/histogram/test/components/AnimatedBarSeries.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow, mount, render } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import { Bar } from '@vx/shape';
 import { NodeGroup } from 'react-move';
 
