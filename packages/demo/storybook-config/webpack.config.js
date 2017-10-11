fd80c5ee392913e7c6f65cf0e5dd8a87e15ae42b
Chris Williams <chris.williams@airbnb.com>
2017-10-11 12:59:36 -0700

    [build] remove unnecessary webpack flags, fix react errors in published storybook
    [deps] remove more unnecessary flags

--- packages/demo/storybook-config/webpack.config.js
+++ packages/demo/storybook-config/webpack.config.js
@@ -1,9 +1,11 @@
 /* eslint no-param-reassign: 0 */
 // Export a function instead of an object, the function will accept the base config:
 module.exports = (storybookConfig) => {
-  storybookConfig.plugins = storybookConfig.plugins.filter(plugin => (
-    !(/uglifyjs/i).test(plugin.constructor.name) // filter out UglifyJS
-  ));
+  if (process.env.NODE_ENV !== 'production') {
+    storybookConfig.plugins = storybookConfig.plugins.filter(plugin => (
+      !(/uglifyjs/i).test(plugin.constructor.name) // filter out UglifyJS
+    ));
+  }
 
   storybookConfig.module.rules.push(
     {
