08bd5141692889292017c77623b141519f3d590a
Chris Williams <chris.williams@airbnb.com>
2018-08-31 19:27:59 -0700

    [xy-chart] remove tests that linting can do

--- packages/xy-chart/test/aesthetic/LinearGradient.test.js
+++
@@ -1,8 +0,0 @@
-import { LinearGradient } from '../../src';
-
-describe('<LinearGradient />', () => {
-  // this is re-exported from vx so is the only test we add
-  it('should be defined', () => {
-    expect(LinearGradient).toBeDefined();
-  });
-});
--- packages/xy-chart/test/enhancer/ParentSize.test.js
+++
@@ -1,8 +0,0 @@
-import { ParentSize } from '../../src';
-
-describe('<ParentSize />', () => {
-  // this is re-exported from vx so is the only test we add
-  it('should be defined', () => {
-    expect(ParentSize).toBeDefined();
-  });
-});
